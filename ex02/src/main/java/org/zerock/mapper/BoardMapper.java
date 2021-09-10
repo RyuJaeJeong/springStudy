7cc8d9b0c9ef069b292069c21d7e40157b8851a3
RyuJaeJeong <fb0982@naver.com>
2021-09-10 23:46:27 +0900

    영속/비즈니스 계층의 crud구현

---
+++ ex02/src/main/java/org/zerock/mapper/BoardMapper.java
@@ -0,0 +1,13 @@
+package org.zerock.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Select;
+import org.zerock.domain.BoardVO;
+
+public interface BoardMapper {
+	
+	
+	public List<BoardVO> getList();
+	
+}
