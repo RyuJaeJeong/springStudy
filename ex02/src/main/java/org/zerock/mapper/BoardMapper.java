e8170bb35cd28230397c8104930c5695babd51aa
RyuJaeJeong <fb0982@naver.com>
2021-09-11 17:25:40 +0900

    비즈니스 계층

--- ex02/src/main/java/org/zerock/mapper/BoardMapper.java
+++ ex02/src/main/java/org/zerock/mapper/BoardMapper.java
@@ -10,4 +10,15 @@ public interface BoardMapper {
 	
 	public List<BoardVO> getList();
 	
+	public void insert(BoardVO board);
+	
+	public void insertSelectKey(BoardVO board); //자동으로 추가되는 pk값을 확인해야할때.
+	
+	public BoardVO read(long bno);
+	
+	public int delete(long bno);
+	
+	public int update(BoardVO board);
+	
+	
 }
